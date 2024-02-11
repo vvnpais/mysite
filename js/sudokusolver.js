714b87c067612700c51c1ae1a310da105ce78053
Vivian <210010034@iitdh.ac.in>
2024-02-11 20:55:43 +0530

    chess fixed

--- js/sudokusolver.js
+++ js/sudokusolver.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const s=document.querySelectorAll('.grid div');
-    const gameButton=document.getElementById("game");
-    const solverButton=document.getElementById("solver");
+    const gameButton=document.getElementById("gameButton");
+    const solverButton=document.getElementById("solverButton");
     const endButton=document.getElementById("endButton");
     gameButton.classList.add("selectedButton");
+    let clickIndex=null;
+    let game=Array(81).fill(0);
+
+    //move higlight through touchpad and click
+    for(let i=0;i<81;i++){
+        s[i].addEventListener('click',()=>{
+            if(clickIndex!=null){s[clickIndex].classList.remove("yellow");}
+            s[i].classList.add("yellow");
+            clickIndex=i;
+        })
+    }
+   
+    //keyboard control
+    document.addEventListener('keydown',(e)=>{takeInput(clickIndex,e)});
     
     let type="game";
 
+    //solverButton
     solverButton.addEventListener("click",()=>{
         type="solver";
         gameButton.classList.remove("selectedButton");
@@ -14,6 +29,8 @@ document.addEventListener('DOMContentLoaded',()=>{
         endButton.textContent="Solve";
         // console.log(type);
     })
+
+    //gameButton
     gameButton.addEventListener("click",()=>{
         type="game";
         solverButton.classList.remove("selectedButton");
@@ -21,7 +38,11 @@ document.addEventListener('DOMContentLoaded',()=>{
         endButton.textContent="Check";
         // console.log(type);
     })
+
+    //endButton
     endButton.addEventListener("click",solveGame);
+    
+    //move highlight with keyboard
     document.addEventListener("keydown",(e)=>{moveHighlight(e)});
 
     const inputs=[
@@ -47,15 +68,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 // helper functions/////////////
 
-    function convert2DTo1D(x,y){
-        return y*9+x;
-    }
-
-    function convert1DTo2D(index){
-        let x=index%9;
-        let y=Math.floor(index/9);
-    }
-
     function duplicateInArray(arr){
         for(let i=0; i<arr.length; i++){
             if(arr.indexOf(arr[i])!==arr.lastIndexOf(arr[i])){
@@ -80,13 +92,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 ////////////////////////////////
 
-let originalGame=Array(81).fill(0), n=Array(81).fill(0);
-let order=null, game=null;
-let add="add",remove="remove";
-let clickIndex=null;
-
-// other functions
-
+    //function to move highlight through keyboard
     function moveHighlight(e){
         if(e.keyCode>36 && e.keyCode<41){
             if(clickIndex==null){
@@ -103,8 +109,9 @@ let clickIndex=null;
         }
     }
 
+    //function to check logic, fills half the board correctly
     function halfComplete(){
-        originalGame=[1, 2, 3, 4, 5, 6, 7, 8, 9, 
+        game=[1, 2, 3, 4, 5, 6, 7, 8, 9, 
             4, 5, 6, 7, 8, 9, 1, 2, 3, 7, 8, 9, 
             1, 2, 3, 4, 5, 6, 2, 3, 1, 5, 6, 4, 
             8, 9, 7, 5, 6, 4, 8, 9, 7, 2, 3, 1, 
@@ -113,198 +120,32 @@ let clickIndex=null;
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
 
+    //creates array of index of empty cells and shuffles array
     function createOrder(){
         order=[];
-        for(let i=0;i<81;i++){if(originalGame[i]==0){order.push(i);}}
+        for(let i=0;i<81;i++){if(game[i]==0){order.push(i);}}
         order.sort( () => (Math.random()-0.5) );
         console.log(order);
     }
 
+    //input from keyboard for game board
     function takeInput(i,e){
         if(inputs.includes(e.code)){
-            update(i,parseInt(e.key),add);
-            s[i].textContent=parseInt(e.key);
-            originalGame[i]=parseInt(e.key);
+            s[i].textContent=Number(e.key);
+            game[i]=Number(e.key);
         }
-        else if(e.keyCode==8){
-            update(i,parseInt(s[i].textContent),remove);
+        else if(e.keyCode==8 || e.keyCode==48 || e.keyCode==32){
             s[i].textContent="";
-            originalGame[i]=0;
+            game[i]=0;
         }
-        createGame();
-        console.log(originalGame);
-        console.log(checkGridSafety());
+        console.log(game);
     }
 
-    function createGame(){
-        game=originalGame.slice();
-    }
-
-    
-    function update(index,number,op){
-        let column=index%9;
-        let row=Math.floor(index/9);
-        let xCenterOfBlock=Math.floor(column/3)*3+1;
-        let yCenterOfBlock=Math.floor(row/3)*3+1;
-        if(op==add){
-            for(let i=row*9;i<(row+1)*9;i++){n[i][number]=0;}
-            for(let i=0;i<9;i++){n[i*9+column][number]=0;}
-            for(let x=xCenterOfBlock-1;x<=xCenterOfBlock+1;x++){
-                for(let y=yCenterOfBlock-1;y<=yCenterOfBlock+1;y++){n[convert2DTo1D(x,y)][number]=0;}
-            }
-        }
-        else if(op==remove){
-            for(let i=row*9;i<(row+1)*9;i++){n[i][number]=1;}
-            for(let i=0;i<9;i++){n[i*9+column][number]=1;}
-            for(let x=xCenterOfBlock-1;x<=xCenterOfBlock+1;x++){
-                for(let y=yCenterOfBlock-1;y<=yCenterOfBlock+1;y++){n[convert2DTo1D(x,y)][number]=1;}
-            }
-        }
-        // console.log(n);
-    }
-    
-    function checkGridSafety(){
-        for(let i=0;i<9;i++){
-            let checkArrayRow=[];
-            let checkArrayColumn=[];
-            for(let j=0;j<9;j++){
-                if(game[i*9+j]!=0){checkArrayRow.push(game[i*9+j]);}
-                if(game[j*9+i]!=0){checkArrayColumn.push(game[j*9+i]);}
-            }
-            if(duplicateInArray(checkArrayRow) || duplicateInArray(checkArrayColumn)){return false;}
-        }
-        for(let a=1;a<8;a=a+3){
-            for(let b=1;b<8;b=b+3){
-                let checkArrayBlock=[];
-                let xCenterOfBlock=a;
-                let yCenterOfBlock=b;
-                for(let x=xCenterOfBlock-1;x<=xCenterOfBlock+1;x++){
-                    for(let y=yCenterOfBlock-1;y<=yCenterOfBlock+1;y++){
-                        if(game[convert2DTo1D(x,y)]!=0){checkArrayBlock.push(game[convert2DTo1D(x,y)]);}
-                    }
-                }
-                if(duplicateInArray(checkArrayBlock)){
-                    // console.log("unsafe");
-                    return false;
-                }
-            }
-        } 
-        return true;
-    }
-    
-    function lastAddedNumberSafety(order,index){
-        let column=order[index]%9;
-        let row=Math.floor(order[index]/9);
-        let xCenterOfBlock=Math.floor(column/3)*3+1;
-        let yCenterOfBlock=Math.floor(row/3)*3+1;
-        for(let i=row*9;i<(row+1)*9;i++){
-            if(i==order[index]){continue;}
-            else{
-                if(game[i]==game[order[index]]){
-                return false;
-                }
-            }
-        }
-        for(let i=0;i<9;i++){
-            if((i*9+column)==order[index]){continue;}
-            else{
-                if(game[i*9+column]==game[order[index]]){
-                    return false;
-                }
-            }
-        }
-        for(let x=xCenterOfBlock-1;x<=xCenterOfBlock+1;x++){
-            for(let y=yCenterOfBlock-1;y<=yCenterOfBlock+1;y++){
-                if((convert2DTo1D(x,y))==order[index]){continue;}
-                else{
-                    if(game[convert2DTo1D(x,y)]==game[order[index]]){
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    
-    function gameComplete(){
-        let filledBoxes=0;
-        for(let i=0;i<81;i++){
-            if(game[i]!=0 && game[i]>0 && game[i]<10){
-                filledBoxes+=1;
-            }
-        }
-        if(filledBoxes==81 && checkGridSafety()){
-            return true;
-        }
-        return false;
-    }
-    
-    function backtrack(order,n1,n2){
-        let i=0;
-        while(!gameComplete()){
-            i+=1;
-            if(i==20000000){
-                createGame();
-                createOrder();
-                n1=0;
-                n2=1;
-                i=0;
-            }
-            if(n1>=order.length){console.log("Cannot be done"); console.log(checkGridSafety()); return false;}
-            else{
-                console.log(n1,n2);
-                game[order[n1]]=n2;
-                if(!lastAddedNumberSafety(order,n1)){
-                    if(n2>8){
-                        game[order[n1]]=0;
-                        n1-=1; n2=game[order[n1]]+1;
-                        while(n2>8){
-                            game[order[n1]]=0;
-                            n1-=1; n2=game[order[n1]]+1;
-                        }
-                    }
-                    else{
-                        game[order[n1]]=0;
-                        n2+=1;
-                    }
-                }
-                else{
-                    n1+=1;
-                    n2=1;
-                }
-            }
-        }
-        updateBoard();
-        return true;
-    }
-
+    //function to solve board
     function solveGame(){
-        // console.log(game);
-        // interval=setInterval(()=>{
-        //     cInterval=true;
-        //     cInterval=false;
-        //     if(gameComplete){clearInterval(interval);}
-        //     for(let i=0;i<81;i++){
-        //         s[i].textContent="";
-        //     }
-        //     backtrack(order,0,1);
-        // },2000);
-        // backtrack(order,0,1);
         halfComplete();
-        createGame();
         createOrder();
         console.log(game);
-        backtrack(order,0,1);
     }
-    
-    for(let i=0;i<81;i++){
-         s[i].addEventListener('click',()=>{
-             if(clickIndex!=null){s[clickIndex].classList.remove("yellow");}
-             s[i].classList.add("yellow");
-             clickIndex=i;
-         })
-     }
-    
-     document.addEventListener('keydown',(e)=>{takeInput(clickIndex,e)});
     
 })
